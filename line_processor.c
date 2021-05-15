56bdfd9d75e261cc6c2a3a6bb7bdf8f3df4a6833
Mateo EstradaJorge <estradma@os1.engr.oregonstate.edu>
2021-05-14 19:39:28 -0700

    Added the second thread.

--- line_processor.c
+++ line_processor.c
@@ -9,7 +9,7 @@
 
 // size of the buffer
 #define SIZE 1000 // fifty lines of 1000 characters each
-#define LINES_SIZE 80 // line output are 80 characters long
+#define INPUT_LINES_SIZE 50 // line output are 80 characters long
 // Special marker used to indicate end of the producer data
 
 // special marker used to indicate end of the producer data
@@ -24,7 +24,8 @@ enum
 
 
 // Flags for while loops with input
-bool flag_stop = true;
+bool flag_stop = false;
+bool flag_separator = false;
 
 
 // strcmp const
@@ -34,7 +35,8 @@ const char *stop = "STOP\n";
 
 
 // Buffer 1, shared resource between input thread and line separator thread
-char buffer_1[SIZE];
+char buffer_1[INPUT_LINES_SIZE][SIZE];
+// Number of lines in the buffer
 int count_1 = 0;
 // index where input thread will put the next item
 int prod_idx_1 = 0;
@@ -64,6 +66,16 @@ pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full_2 = PTHREAD_COND_INITIALIZER;
 
 
+void print_buffer(char buffer[]){
+  
+    printf("[%s]", buffer);
+    
+}
+
+
+
+
+
 /*
     This function is used to pass the string from the fgets() to the buffer_1 from one line till it hits
     the newline character.
@@ -72,10 +84,16 @@ pthread_cond_t full_2 = PTHREAD_COND_INITIALIZER;
 void put_buff_1(char buf_1[], int count){
     // lock the mutual exclusion code 
     pthread_mutex_lock(&mutex_1); 
+    // printf("[%s]", buf_1);
 
     // place the string into the buffer at the right index
     strcpy(buffer_1[prod_idx_1], buf_1);
+    count_1++;
 
+    // printing the content in the buffer_1
+    // print_buffer(buffer_1[prod_idx_1]);
+    // fflush(stdout);
+    
     // increment the prod_idx
     prod_idx_1 = prod_idx_1 + 1;
 
@@ -84,11 +102,6 @@ void put_buff_1(char buf_1[], int count){
 
     // unlock the mutual exclusion code
     pthread_mutex_unlock(&mutex_1);
-}
-
-
-void print_buffer(char buffer[]){
-    printf("[%s]", buffer);
 }
 
 /*
@@ -101,29 +114,49 @@ void print_buffer(char buffer[]){
 
 void *read_input(void *args){
     int count = 0;
-    bool find_stop = false;
-    const char arr_space[2] = " ";
    
-
-    
     // Get the input to the buffer_1
-    while(!find_stop){
-        fgets(buffer_1, SIZE, stdin);
-        
-        // passing the text to the buffer
-        put_buff_1(buffer_1, count);
+    while(!flag_stop){
 
-        if(strcmp(buffer_1, "STOP\n") == 0){
-            find_stop = true; // set the stopper when we reach the word STOP
+        // get the input from the file
+        fgets(buffer_1[count], SIZE, stdin);
+
+
+        if(strcmp(buffer_1[count], stop) == 0){
+            flag_stop = true; // set the stopper when we reach the word STOP
+        }else{
+             // passing the text to the buffer unless it hits STOP
+            put_buff_1(buffer_1[count], count);
+            count++;
         }
-        count++;
+       
     }
-
-    // Checking to see if the buffer does have the string saved
-    print_buffer(buffer_1);
     return NULL;
 }
 
+
+/*
+    This function is used to get the buffer_1 line for the line separator thread.
+    This code is from the sample file: https://replit.com/@cs344/64prodconsunboundsolc#main.c
+*/
+char *get_buff_1(){
+    char *line_array = malloc(SIZE*sizeof(char*));
+    // Lock the mutual exlusion before checking if the buffer_1 has data
+    pthread_mutex_lock(&mutex_1);
+    while(count_1 == 0)
+        // buffer is empty. wait for the producer to signal that the buffer has data
+        pthread_cond_wait(&full_1, &mutex_1);
+    strcpy(line_array, buffer_1[con_idx_1]);
+    // Increment the index from which the item will be picked up
+    con_idx_1 = con_idx_1 + 1;
+    // decrement the number of items in the buffer_1
+    count_1--;
+    // unlock the mutex
+    pthread_mutex_unlock(&mutex_1);
+    //return the line
+    // printf("Does it get here? %s", line);
+    return line_array;
+}
 
 /*
 
@@ -132,19 +165,21 @@ void *read_input(void *args){
 
 */
 
+void *line_separator(void *args){
+    int count = 0;
+    // delcaring a temporary array where the string will be.
+   
 
-void line_separator(void *args){
-
-    // Gotta lock the mutex_1 before checking if buffer_1 has data
-    pthread_mutex_lock(&mutex_1);
-
-    // check if buffer_1 is empty, if it is then wait for the producer to 
-    while(strlen(buffer_1) == 0){
-        printf("This is empty: %s\n", buffer_1);
-
-
-    }
-
+   for (int i = 0; i < INPUT_LINES_SIZE; i++){
+       // get the first line that has a newline at the end
+        char *line = get_buff_1();
+        // printf("%s", line);
+    //    if (strcmp(line[i], '\n') == 0){
+    //        strcpy(line[i], ' ');
+    //    }
+   }
+    // print_buffer(line);
+return NULL;
 
 
 }
@@ -154,23 +189,18 @@ void line_separator(void *args){
 // using the input example from the lecture on inputting files to producer/consumer programs.
 
 int main(int argc, char *argv[]){
-
-    // initialize the buffers to have 0 
-    memset(buffer_1, 0, SIZE);
-    memset(buffer_2, 0, SIZE);
-    // memset(buffer_3, 0, SIZE);
     // This is from the unbounded three threads example from the lectures on conditional variable.
     pthread_t input_t, line_t, output_t;
     // create the threads
     pthread_create(&input_t, NULL, read_input, NULL);
-    // pthread_create(&line_t, NULL, line_separator, NULL);
+    pthread_create(&line_t, NULL, line_separator, NULL);
     // pthread_create(&output_t, NULL, write_output, NULL);
 
 
 
     // join the threads
     pthread_join(input_t, NULL);
-    // pthread_join(line_t, NULL);
+    pthread_join(line_t, NULL);
     // pthread_join(output_t, NULL);
     return EXIT_SUCCESS;
 
