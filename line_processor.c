2e1b2c8930c24e7f8884cb604d5e2bd912dce22b
Mateo EstradaJorge <estradma@os1.engr.oregonstate.edu>
2021-05-15 15:03:28 -0700

    Added pthread_2.

--- line_processor.c
+++ line_processor.c
@@ -53,7 +53,7 @@ pthread_cond_t full_1 = PTHREAD_COND_INITIALIZER;
 
 // Buffer 2, shared resource between line separator thread and the output thread (adding the plus sign thread will start HERE)
 
-char buffer_2[SIZE];
+char buffer_2[INPUT_LINES_SIZE][SIZE];
 // Number of items in the buffer
 int count_2 = 0;
 // Index where the line separator thread will put the next item
@@ -66,15 +66,23 @@ pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full_2 = PTHREAD_COND_INITIALIZER;
 
 
-void print_buffer(char buffer[]){
+// prints array of chars
+void print_buffer_array(char buffer[]){
   
-    printf("[%s]", buffer);
+    printf("[%s]\n", buffer);
     
 }
 
+// prints chars
+void print_buffer_char(char buffer){
+    printf("[%c]", buffer);
+}
 
+// print number of lines in a buffer
 
-
+void print_num_lines(int counts){
+    printf("Number of lines in buffer: [%d]\n", counts);
+}
 
 /*
     This function is used to pass the string from the fgets() to the buffer_1 from one line till it hits
@@ -89,10 +97,6 @@ void put_buff_1(char buf_1[], int count){
     // place the string into the buffer at the right index
     strcpy(buffer_1[prod_idx_1], buf_1);
     count_1++;
-
-    // printing the content in the buffer_1
-    // print_buffer(buffer_1[prod_idx_1]);
-    // fflush(stdout);
     
     // increment the prod_idx
     prod_idx_1 = prod_idx_1 + 1;
@@ -120,8 +124,6 @@ void *read_input(void *args){
 
         // get the input from the file
         fgets(buffer_1[count], SIZE, stdin);
-
-
         if(strcmp(buffer_1[count], stop) == 0){
             flag_stop = true; // set the stopper when we reach the word STOP
         }else{
@@ -140,22 +142,44 @@ void *read_input(void *args){
     This code is from the sample file: https://replit.com/@cs344/64prodconsunboundsolc#main.c
 */
 char *get_buff_1(){
-    char *line_array = malloc(SIZE*sizeof(char*));
+    char *line_array;
+    line_array = malloc(SIZE + 1);
     // Lock the mutual exlusion before checking if the buffer_1 has data
     pthread_mutex_lock(&mutex_1);
     while(count_1 == 0)
+
         // buffer is empty. wait for the producer to signal that the buffer has data
         pthread_cond_wait(&full_1, &mutex_1);
+    
+    // print_buffer_array(buffer_1[con_idx_1]);
+
     strcpy(line_array, buffer_1[con_idx_1]);
     // Increment the index from which the item will be picked up
     con_idx_1 = con_idx_1 + 1;
     // decrement the number of items in the buffer_1
-    count_1--;
+    count_1--; // this will stop the program if there is no more inputs.
     // unlock the mutex
     pthread_mutex_unlock(&mutex_1);
+
     //return the line
-    // printf("Does it get here? %s", line);
+    // print_buffer_array(line_array);
+
     return line_array;
+}
+
+void put_buff_2(char arr[]){
+
+    // Lock the mutex before putting the string into the buffer
+    pthread_mutex_lock(&mutex_2);
+    // Put the string in the buffer_2
+    strcpy(buffer_2[prod_idx_2], arr);
+    // Increment the index where the next string will be put.
+    prod_idx_2 = prod_idx_2 + 1;
+    count_2++; // increase the number of lines in buffer_2 by 1
+    //signal to the consumer that buffer_2 is no longer empty
+    pthread_cond_signal(&full_2);
+    // unlock the mutex
+    pthread_mutex_unlock(&mutex_2); 
 }
 
 /*
@@ -167,27 +191,35 @@ char *get_buff_1(){
 
 void *line_separator(void *args){
     int count = 0;
-    // delcaring a temporary array where the string will be.
-   
+    // set the array pointer so that I can free the malloc() from the get_buf_1() to avoid memory leaks.
+    char *line_array;
+    char arr[SIZE*INPUT_LINES_SIZE];
+    print_num_lines(count_1);
+    // I need to call the get_buff_1() count_1 times since that's how many lines of string
+    // are in buffer_1
+    while(count_1 > 0){
+        line_array = get_buff_1();
+        strncat(arr, line_array, SIZE);
+        // Looping through the strings in the arr[] to find
+        // places with newline characters and replace them with spaces.
+        for (int i = 0; i<SIZE; i++){
+            if (arr[i] == '\n'){
+                arr[i] = ' ';
+                // print_buffer_array(arr);
+            }
+        }
 
-   for (int i = 0; i < INPUT_LINES_SIZE; i++){
-       // get the first line that has a newline at the end
-        char *line = get_buff_1();
-        // printf("%s", line);
-    //    if (strcmp(line[i], '\n') == 0){
-    //        strcpy(line[i], ' ');
-    //    }
-   }
-    // print_buffer(line);
+        // Now that the newlines have been removed, I must add the string to the second buffer_2
+        put_buff_2(arr);
+    }
+    
+    free(line_array);
 return NULL;
-
-
 }
 
 
 
 // using the input example from the lecture on inputting files to producer/consumer programs.
-
 int main(int argc, char *argv[]){
     // This is from the unbounded three threads example from the lectures on conditional variable.
     pthread_t input_t, line_t, output_t;
